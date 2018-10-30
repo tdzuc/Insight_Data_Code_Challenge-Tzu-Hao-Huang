77326e2c7f1f80ab16f4e0794896fb097aca46db
Tzu-Hao Huang <d95522013@gmail.com>
2018-10-30 02:19:07 -0400

    test2

--- insight_testsuite/run_tests.sh
+++ insight_testsuite/run_tests.sh
@@ -2,6 +2,6 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-python ../src/h1b_counting.py ./tests/test_1/input/h1b_input.csv ./tests/test_1/output/top_10_occupations.txt ./tests/test_1/output/top_10_states.txt
+python3 ../src/h1b_counting.py ./tests/test_1/input/h1b_input.csv ./tests/test_1/output/top_10_occupations.txt ./tests/test_1/output/top_10_states.txt
 
-python ../src/h1b_counting.py ./tests/your-own-test_1/input/h1b_input.csv ./tests/your-own-test_1/output/top_10_occupations.txt ./tests/your-own-test_1/output/top_10_states.txt
+python3 ../src/h1b_counting.py ./tests/your-own-test_1/input/h1b_input.csv ./tests/your-own-test_1/output/top_10_occupations.txt ./tests/your-own-test_1/output/top_10_states.txt
